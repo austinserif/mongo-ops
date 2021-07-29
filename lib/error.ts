0abbb8d7faf99d7d2fe8a7c5cb825e8855d75abf
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-07-28 18:40:36 -0700

    added error class

---
+++ lib/error.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * creates a new MongoOpsError
+ * 
+ * **First iteration based on MongoError**, for more info
+ * see [MongoClient node driver on github](https://github.com/mongodb/node-mongodb-native/blob/3.6/lib/core/error.js).
+ * 
+ * @augments Error
+ * @param {Error|string|object} message error message
+ * @property {string} message error message
+ * @property {string} stack error call stack
+ */
+class MongoOpsError extends Error {
+    constructor(message: string | Error) {
+        if (message instanceof Error) {
+            super(message.message);
+            this.stack = message.stack;
+        } else {
+            // the message argument is not an Error structure, is most likely a string in this case
+            // so we should call parent constructor with this message    
+            super(message);
+            
+            // then add null call stack
+            this.stack = undefined;
+        }
+    }
+};
+
+export default MongoOpsError;
