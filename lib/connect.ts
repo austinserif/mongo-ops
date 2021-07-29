f5419c89057c5b977429f8eadacdb985aba625b6
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-07-28 19:08:41 -0700

    added mongo client

--- lib/connect.ts
+++ lib/connect.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+import MongoClient from 'mongodb';
 import MongoOpsError from "./error";
 
 /**
@@ -167,27 +168,24 @@ function buildUriConnectionString(params: ConnectionParams) {
  * connection with the database server, and returns 
  * a connection object.
  */
-// export async function handleClientConnection() {
-//     try {
-//         // require the MongoClient constructor
+export async function handleClientConnection() {
+    try {
 
-//         const MongoClient = require('mongodb').MongoClient;
+        // look for a MONGO_DB_URL on process.env object
+        // throw an error if none found
+        if (!process.env.MONGO_DB_URL || process.env.MONGO_DB_URL === 'undefined') throw new Error('No connection string found in environment variables');
 
-//         // look for a MONGO_DB_URL on process.env object
-//         // throw an error if none found
-//         if (!process.env.MONGO_DB_URL || process.env.MONGO_DB_URL === 'undefined') throw new Error('No connection string found in environment variables');
+        // establish connection
+        const clientConnection = new MongoClient(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//         // establish connection
-//         const clientConnection = new MongoClient(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        return {
+            getConnectionObject() {
+                return clientConnection;
+            }
+        }
+    } catch (err) {
+        if (err instanceof Error) throw new Error(err.message);
 
-//         return {
-//             getConnectionObject() {
-//                 return clientConnection;
-//             }
-//         }
-//     } catch (err) {
-//         if (err instanceof Error) throw new Error(err.message);
-
-//         throw new Error('An unknown error occurred');
-//     }
-// };
+        throw new Error('An unknown error occurred');
+    }
+};
